8d9aef379c152f01c857db460378c312d5a87001
chipsahoying <chrishoying@gmail.com>
2016-06-01 11:32:18 -0500

    99 Bottles of Beer lyric generator
    Simple print command in for loop

---
+++ Firstproject.py
@@ -0,0 +1,11 @@
+'''import random
+import sys
+import os'''
+
+bottletext1 = "Bottles of beer"
+bottletext2 = "on the wall!"
+bottletext3 = "take one down, pass it around,"
+
+for xopp in range(1,100):
+    x =100-xopp
+    print(x,bottletext1,bottletext2,x,bottletext1,"...",bottletext3, x-1, bottletext1, bottletext2, '\n')
